788c1b0b91d41146e81c3ab36e28036650015e79
Chirag Byanjankar <chiragbyanjankar@Chirags-MacBook-Pro.local>
2026-05-07 14:58:19 -0500

    Change background opacity

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -168,7 +168,7 @@ export default function About() {
 
       {/* Community Testimonials */}
       <section className="py-20 text-white parallax" style={{ backgroundImage: "url('/images/026bf747.jfif')" }}>
-        <div className="bg-blue-900 bg-opacity-70 py-20">
+        <div className="bg-blue-900 bg-opacity-40 py-20">
           <div className="container mx-auto px-4">
             <h2 className="text-3xl font-bold text-center mb-12">What Our Community Says</h2>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
